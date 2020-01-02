bae04f80a7b880e41223e50bbcf91f71641cf46a
Iwo Polański <outlying@gmail.com>
2020-01-02 18:21:13 +0100

    Arguments added

--- feed.py
+++ feed.py
@@ -1,3 +1,25 @@
 #!/usr/bin/env python3
+import getopt, sys
 
-print("test")
+argumentList = sys.argv[1:]
+
+unixOptions = "p:"
+gnuOptions = ["page="]
+
+try:
+    arguments, values = getopt.getopt(argumentList, unixOptions, gnuOptions)
+except getopt.error as err:
+    # output error, and return with an error code
+    print (str(err))
+    sys.exit(2)
+
+page = None
+
+for currentArgument, currentValue in arguments:
+    if currentArgument in ("-p", "--page"):
+        page = currentValue
+
+if page is None:
+	raise Exception('Missing -p --page parameter')
+
+
